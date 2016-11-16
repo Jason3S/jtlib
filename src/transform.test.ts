7e8cb8c22f3d5d39a9e860bdcdbbedd9171a62da
Jason Dent <jason@streetsidesoftware.nl>
2016-11-16 21:40:13 +0100

    Add some tests.

--- src/transform.test.ts
+++ src/transform.test.ts
@@ -16,5 +16,32 @@ describe('Transform', () => {
         const fnParent = Transform.field('name').parent();
         expect(fnParent(json)).to.be.equal(json);
     });
+
+    it('expects that the parent still exists even though the field does not.', () => {
+        const json = {name: 'First'};
+        const fnParent = Transform.field('age').parent();
+        expect(fnParent(json)).to.be.equal(json);
+    });
+
+    it('expects parent result to be undefined.', () => {
+        const json = {name: 'First'};
+        const fnParent = Transform.parent();
+        expect(fnParent(json)).to.be.undefined;
+    });
+
+    it('expects parent result to be undefined.', () => {
+        const json = {name: 'First'};
+        const fnParent = Transform.field('name').parent().parent();
+        expect(fnParent(json)).to.be.undefined;
+    });
+
+    it('extract field should give the right value.', () => {
+        const json = { a: { b: { c: 'abc' }}, a1: { b: 'a1b'} };
+        const fnField0 = Transform.field('a').field('b').field('c');
+        const fnField1 = fnField0.parent().parent().parent().field('a1').field('b');
+        expect(fnField0(json)).to.be.equal('abc');
+        expect(fnField1(json)).to.be.equal('a1b');
+    });
+
 });
 
